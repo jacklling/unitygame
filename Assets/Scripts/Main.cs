4a74f25f35c6903f0abcb248cb9122810d046169
huangducai <465648890@qq.com>
2017-11-21 16:19:22 +0800

    first test

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.lib;
+using System.IO;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //UnityHelper instance = UnityHelper.GetInstance();
+        //string writepath = Path.Combine(Application.persistentDataPath, "userdata");
+        //instance.CreateFile(writepath, "mydata.txt", "this is a test");
+
+        //ArrayList list = instance.ReadFile(writepath);
+        //for (int i = 0; i < list.Count; i++)
+        //{
+        //    Debug.Log("data is :" + list[i]);
+        //}
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/lib/UnityHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.lib
+{
+    class UnityHelper
+    {
+        static string Logpre = "UnityHelper";
+        static UnityHelper helpInstance = null;
+        public void CreateDir(string path) {
+
+
+        }
+
+        public static UnityHelper GetInstance() {
+            if (UnityHelper.helpInstance == null) {
+                UnityHelper.helpInstance = new UnityHelper();
+            }
+            return UnityHelper.helpInstance;
+        }
+
+        public void CreateFile(string path, string filename, string info) {
+            StreamWriter sw;
+            string endpath = Path.Combine(path, filename);
+            FileInfo f = new FileInfo(endpath);
+            if (!f.Exists) {
+                sw = f.CreateText();
+            }
+            else
+            {
+                sw = f.AppendText(); 
+            }
+            sw.Write(info);
+            sw.Close();
+            sw.Dispose();
+        }
+
+        public void DeleteFile(string path) {
+            FileInfo f = new FileInfo(path);
+            if (f.Exists){
+                File.Delete(path);
+            }
+        }
+
+        public ArrayList ReadFile(string path) {
+            ArrayList array = new ArrayList();
+            FileInfo f = new FileInfo(path);
+            if (f.Exists)
+            {
+                StreamReader sr = f.OpenText();
+                string line = sr.ReadLine();
+                while (line != null) {
+                    array.Add(line);
+                    line = sr.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine(Logpre + "ReadFile: " + path + "not exists");
+            }
+            return array;
+        }
+    }
+}
